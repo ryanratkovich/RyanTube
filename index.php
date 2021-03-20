3616796d3adfc604de5b4769be95d5d67ac276d3
ryanratkovich <ryanratkovich@gmail.com>
2021-03-20 17:29:17 -0400

    Add files

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>RyanTube</title>
+	
+	<!-- CSS only -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+	<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+<script src="assets/js/commonActions.js"></script>
+
+</head>
+<body>
+	
+	<div id="pageContainer">
+		
+		<div id="mastHeadContainer">
+			<button class="navShowHide">Click me</button>
+		</div>
+
+		<div id="sideNavContainer" style="display:none;">
+			
+		</div>
+
+		<div id="mainSectionContainer">
+			
+			<div id="mainContentContainer">
+				hello 2
+			</div>
+
+		</div>
+
+	</div>
+
+</body>
+</html>

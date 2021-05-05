ba1e55afd1d9dc0eb698f9c77ef629d38cf2dcdc
ryanratkovich <ryanratkovich@gmail.com>
2021-05-05 18:51:58 -0400

    Navigation Bars Added

---
+++ includes/header.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>RyanTube</title>
+	
+	<!-- CSS only -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+	<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+<script src="assets/js/commonActions.js"></script>
+
+</head>
+<body>
+	
+	<div id="pageContainer">
+		
+		<div id="mastHeadContainer">
+			<button class="navShowHide">
+				<img src="assets/images/icons/menu.png">
+			</button>
+
+			<a class="logoContainer" href="index.php">
+				<img src="assets/images/icons/icons8-youtube-100.png" title="logo" alt="Site logo">
+			</a>
+
+			<div class="searchBarContainer">
+				<form action="search.php" method="GET">
+					<input type="text" class="searchBar" name="term" placeholder="Search"></input>
+					<button class="searchButton">
+						<img src="assets/images/icons/search.png">
+					</button>
+				</form>
+			</div>
+
+			<div class="rightIcons">
+				<a href="upload.php">
+					<img class="upload" src="assets/images/icons/upload.png">
+				</a>
+				<a href="#">
+					<img class="upload" src="assets/images/profilePictures/default.png">
+				</a>
+			</div>
+
+		</div>
+
+		<div id="sideNavContainer" style="display:none;">
+			
+		</div>
+
+		<div id="mainSectionContainer">
+			
+			<div id="mainContentContainer">
--- index.php
+++ index.php
@@ -1,52 +1,7 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>RyanTube</title>
-	
-	<!-- CSS only -->
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+<?php require_once("includes/header.php"); ?>
 
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-	<!-- JavaScript Bundle with Popper -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
-<script src="assets/js/commonActions.js"></script>
 
-</head>
-<body>
-	
-	<div id="pageContainer">
-		
-		<div id="mastHeadContainer">
-			<button class="navShowHide">
-				<img src="assets/images/icons/menu.png">
-			</button>
+hello!
 
-			<a class="logoContainer" href="index.php">
-				<img src="assets/images/icons/icons8-youtube-100.png" title="logo" alt="Site logo">
-			</a>
 
-			<div class="searchBarContainer">
-				<form action="search.php" method="GET">
-					<input type="text" class="searchBar" name="term" placeholder="Search"></input>
-				</form>
-			</div>
-
-		</div>
-
-		<div id="sideNavContainer" style="display:none;">
-			
-		</div>
-
-		<div id="mainSectionContainer">
-			
-			<div id="mainContentContainer">
-				hello 2
-			</div>
-
-		</div>
-
-	</div>
-
-</body>
-</html>
+<?php require_once("includes/footer.php"); ?>
